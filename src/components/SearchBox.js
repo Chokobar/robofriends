a5a4463d7c076e64b667aecb0304927808adf7bb
Thanawat Panomnoptham <>
2018-04-09 14:00:24 +0700

    react application first commit

---
+++ src/components/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'tachyons';
+
+const SearchBox = ({ searchField, searchChange}) => {
+	return (
+		<div className='pa2'>
+			<input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='serach robots' onChange={searchChange}/>
+		</div>
+	)
+}
+
+export default SearchBox
